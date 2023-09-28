11f7c5cd202f32691c27b4317914b854d9da1dda
Abdallah Mohsen <93709847+abdallahMohsen3162@users.noreply.github.com>
2023-09-28 22:48:26 +0300

    Add files via upload

---
+++ Topics/datastructures/Trie.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define F first
+#define S second
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define ln cout << "\n";
+#define all(v) v.begin(), v.end()
+#define pr(x) cout << x;
+const int N = 1e5 + 10;
+
+
+
+class Trie{
+class Node{
+public:
+    map<char, Node*>mp;
+    bool leaf;
+    int pref;
+    Node(){
+        leaf = false;
+        pref = 0;
+    }
+};
+public:
+    
+    Node *root;
+    string mx_pref;
+    Trie(){ 
+        root = new Node();
+        mx_pref = "";
+    }
+    void insert(string str){
+        Node* temp = root;
+        for(int i=0;i<str.size();i++){
+            char c = str[i];
+            if(temp->mp.find(c) == temp->mp.end()){
+                temp->mp[c] = new Node();
+            }
+            temp->pref++;
+            temp = temp->mp[c];
+        }
+        temp->leaf = true;
+    }
+
+    bool search(string str){
+        Node*temp = root;
+        for(int i=0;i<str.size();i++){
+            temp = temp->mp[str[i]];
+
+            if(!temp){
+                return false;
+            }
+        }
+        return temp->leaf == true;
+    }
+
+    void maxPref(Node*temp, string cur_str, int sz){
+        if(cur_str.size() > mx_pref.size()){
+            mx_pref = cur_str;
+        }
+        if(temp == nullptr) return;
+         for(char c='a';c<='z';c++){
+            if(temp->pref == sz){
+                maxPref(temp->mp[c], cur_str + c, sz);
+            }
+         }
+    }
+
+};
+
+
+void tc(){
+    Trie *tr = new Trie();
+    tr->insert("abdul");
+    cout << tr->search("abduld");
+
+}
+
+int32_t main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    // cin>>t;
+    while(t--) tc();
+    return 0;
+}
