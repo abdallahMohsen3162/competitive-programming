a47fc9cacb230d53cb3cf95a323dfd937fca9cdf
Abdallah Mohsen <93709847+abdallahMohsen3162@users.noreply.github.com>
2023-10-28 10:01:31 +0200

    Create main.cpp
    main template

---
+++ Topics/main.cpp
@@ -0,0 +1,43 @@
+/*
+*
+* @author: Abdllah mohsen
+*
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define F first
+#define S second
+#define pb push_back
+#define pp pop_back
+#define yes cout << "Yes\n";
+#define no cout << "No\n";
+#define all(stl) stl.begin(), stl.end()
+#define mem(DATASTRUC, VAL) memset(DATASTRUC,  VAL, sizeof(DATASTRUC))
+#define debug(xx) cout << (#xx) << " = " << xx << endl;
+#define ll long long
+#define sprt cout << "--------------------\n";
+using namespace std;
+using namespace __gnu_pbds;
+void fio(){
+	ios_base::sync_with_stdio(0);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+}
+// find_by_order, order_of_key
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set; 
+
+
+
+ 
+void tc() {}
+
+int32_t main(){
+	// fio();
+  int tt = 1; 
+  // cin >> tt;
+  while(tt--)tc();
+  return 0;
+}
